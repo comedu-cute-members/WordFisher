876519ed32ca64befa5d4af9d23ba11511718fc4
eunzee <eunjiamy@naver.com>
2023-11-14 07:48:22 +0900

    flexible video area

--- components/videoarea.jsx
+++ components/videoarea.jsx
@@ -1,8 +1,8 @@
 export default function VideoArea() {
   // call api
   return (
-    <div className="flex-auto bg-blue-400 max-w-[800px] h-fit flex flex-col">
-      <div className="h-[400px] bg-blue-200">I am video area</div>
+    <div className="w-1/2 bg-blue-400 h-fit flex flex-col">
+      <div className="h-[calc(50vw*9/16)] bg-blue-200">I am video area</div>
       <div className="h-[150px] bg-blue-100">I am chart area</div>
     </div>
   );
